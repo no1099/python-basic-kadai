fa8d3d1783ab9d213a918aa6003f031bde417079
no1099 <no1099ts@gmail.com>
2023-07-09 19:49:29 +0900

    kadai_011の提出

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,12 @@
+list_names = ["水","金","地","火","木","土","天","海","冥"]
+
+for name in list_names:
+  print(name)
+
+print("----------------------------------")
+
+num = 0
+max = len(list_names)
+while num < max:
+  print(list_names[num])
+  num = num + 1
